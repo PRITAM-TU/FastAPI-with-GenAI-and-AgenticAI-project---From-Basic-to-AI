16917b7ea6fa30517103e40876301347a3baa1a6
Pritam Tung <pritamtung03@gmail.com>
2026-08-22 19:15:45 +0530

    fix(pydantic):add basemodel for data validation

--- yatar_agetns/models/Response_model.py
+++ yatar_agetns/models/Response_model.py
@@ -4,3 +4,14 @@ from datetime import date
 
 class ResponseModel(BaseModel):
     message:str
+class place_find(BaseModel):
+    name: str
+    city: str
+    description: str
+    rating: float = 0.0
+class TripRequest(BaseModel):
+    destination: str | None=None
+    start_date: date | None=None
+    end_date: date   | None=None
+    currency: str    | None=None
+
