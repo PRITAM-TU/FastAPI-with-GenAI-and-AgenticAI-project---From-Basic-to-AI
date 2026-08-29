aa15ed608b19971cdff7dbc972333941cd5e54eb
Pritam Tung <pritamtung03@gmail.com>
2026-08-29 20:46:32 +0530

    formate code structure

--- MVC/controllers/products_details.js
+++ MVC/controllers/products_details.js
@@ -1,49 +1,47 @@
-const Product_detail = require('../models/products');
+const Product_detail = require("../models/products");
 
-//for get all product details 
+//for get all product details
 const get_all_products = async (_req, res) => {
-    try {
-        const get_product = await Product_detail.find();
-        if (!get_product || get_product.length === 0) {
-            res.status(404).json({
-                success: true,
-                message: "Data not found"
-            });
-            return;
-        }
-        res.status(200).json({
-            success: true,
-            message: get_product
-        });
+  try {
+    const get_product = await Product_detail.find();
+    if (!get_product || get_product.length === 0) {
+      res.status(404).json({
+        success: true,
+        message: "Data not found",
+      });
+      return;
     }
-    catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    }
+    res.status(200).json({
+      success: true,
+      message: get_product,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 };
 
-//post data in database 
+//post data in database
 const create_product = async (req, res) => {
-    try {
-        const {title,description,price,category,stock,images,isAvailable}=req.body();
-        const newproduct= new Product_detail({})
-        res.status(201).json({
-            success: true,
-            message: product
-        });
-    }
-    catch (err) {
-        res.status(400).json({
-            success: false,
-            message: err.message
-        });
-    }
+  try {
+    const { title, description, price, category, stock, images, isAvailable } =
+      req.body();
+    const newproduct = new Product_detail({});
+    res.status(201).json({
+      success: true,
+      message: product,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
 };
-
 
 module.exports = {
-    get_all_products,
-    create_product
+  get_all_products,
+  create_product,
 };
